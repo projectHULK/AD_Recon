a6a2869a95761c3a8080aefe7b63ff6f7085c894
Iron Hulk <65414049+projectHULK@users.noreply.github.com>
2022-09-11 06:06:40 -0400

    Add files via upload

---
+++ ldapdomaindump/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+setup(name='ldapdomaindump',
+      version='0.9.3',
+      description='Active Directory information dumper via LDAP',
+      author='Dirk-jan Mollema',
+      author_email='dirkjan@sanoweb.nl',
+      url='https://github.com/dirkjanm/ldapdomaindump/',
+      packages=['ldapdomaindump'],
+      install_requires=['dnspython', 'ldap3>=2.5,!=2.5.2,!=2.5.0,!=2.6', 'future'],
+      package_data={'ldapdomaindump': ['style.css']},
+      include_package_data=True,
+      scripts=['bin/ldapdomaindump', 'bin/ldd2bloodhound', 'bin/ldd2pretty'],
+      license="MIT",
+      )
